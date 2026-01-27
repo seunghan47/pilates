dfa5e7f74ba8f25926a841ea64c1d0430d1305ea
Paul Lim <paulslim47@gmail.com>
2026-01-26 21:51:26 -0500

    another one

---
+++ src/main/java/com/polim/pilates/pilates/PilatesApplication.java
@@ -0,0 +1,13 @@
+package com.polim.pilates.pilates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polim/pilates/pilates/PilatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polim.pilates.pilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
