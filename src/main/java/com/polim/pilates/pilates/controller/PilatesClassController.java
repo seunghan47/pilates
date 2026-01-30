89d841a0dfa3b9619b3dbaef55d6bd09dc565994
Paul Lim <paulslim47@gmail.com>
2026-01-29 20:40:58 -0500

    fixed some import issues

--- src/main/java/com/polim/pilates/pilates/controller/PilatesClassController.java
+++ src/main/java/com/polim/pilates/pilates/controller/PilatesClassController.java
@@ -1,19 +1,24 @@
 package com.polim.pilates.pilates.controller;
 
+import com.polim.pilates.pilates.entity.PilatesClass;
+import com.polim.pilates.pilates.service.PilatesClassService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/classes")
 public class PilatesClassController {
 
-    private final ClassService classService;
+    private final PilatesClassService classService;
 
-    public PilatesClassController(ClassService classService) {
+    public PilatesClassController(PilatesClassService classService) {
         this.classService = classService;
     }
 
-    @GetMapping
+    @GetMapping("/get")
     public List<PilatesClass> getAllClasses() {
         return classService.findAllAvailableClasses();
     }
--- src/main/java/com/polim/pilates/pilates/service/PilatesClassService.java
+++ src/main/java/com/polim/pilates/pilates/service/PilatesClassService.java
@@ -1,14 +1,19 @@
 package com.polim.pilates.pilates.service;
 
+import com.polim.pilates.pilates.entity.PilatesClass;
+import com.polim.pilates.pilates.repository.PilatesClassRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PilatesClassService {
     private final PilatesClassRepository classRepository;
 
-    public ClassService(PilatesClassRepository classRepository) {
+    public PilatesClassService(PilatesClassRepository classRepository) {
         this.classRepository = classRepository;
     }
+
 
     public List<PilatesClass> findAllAvailableClasses() {
         return classRepository.findAll();
